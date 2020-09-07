630bec98eae8ce6178368c59dc9e9af028ce8f46
Jan Kimr <2017-e-kimr@ad.gyarab.cz>
2020-09-07 08:42:12 +0200

    first commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author 2017-e-kimr
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget mi aliquet, consequat quam at, sodales velit. Nullam vehicula nulla purus, sit amet pretium velit gravida non. Aenean lacinia pharetra volutpat. Duis eget consectetur quam. Ut ut pharetra turpis. Vestibulum mi justo, dictum nec ultrices ut, fermentum ultrices lectus. Maecenas viverra ligula ut tellus accumsan, id euismod risus semper. Duis vitae dignissim mauris. Nulla a est eu diam consectetur mattis. Vestibulum vulputate est non tempus semper. Donec posuere magna ligula, nec suscipit eros egestas at. Vivamus malesuada quis urna ac vulputate.");
+    }
+}
